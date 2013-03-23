f59faa069c816880dbdaab3b1c3e68718669099f
Cody Lundquist <cody.lundquist@gmail.com>
2013-03-23 16:34:22 +1100

    Adding new a couple new filters for images so some effects can applied to them.

---
+++ src/Munee/Asset/Filter/Image/Grayscale.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Munee: Optimising Your Assets
+ *
+ * @copyright Cody Lundquist 2013
+ * @license http://opensource.org/licenses/mit-license.php
+ */
+
+namespace Munee\Asset\Filter\Image;
+
+use Munee\Asset\Filter;
+use Imagine\Gd\Imagine;
+
+/**
+ * Grayscale Filter for Images
+ *
+ * @author Cody Lundquist
+ */
+class Grayscale extends Filter
+{
+    /**
+     * @var array
+     */
+    protected $_allowedParams = array(
+        'grayscale' => array(
+            'regex' => 'true|false|t|f|yes|no|y|n',
+            'default' => 'false',
+            'cast' => 'boolean'
+        )
+    );
+
+    /**
+     * Turn an image Grayscale
+     *
+     * @param string $file
+     * @param array $arguments
+     *
+     * @return void
+     */
+    public function doFilter($file, $arguments)
+    {
+        if (! $arguments['grayscale']) {
+            return;
+        }
+
+        $Imagine = new Imagine();
+        $image = $Imagine->open($file);
+        $image->effects()->grayscale();
+        $image->save($file);
+    }
+}

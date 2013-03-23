4d17e42c023570e28682248da871903704e6fb60
Cody Lundquist <cody@4mation.com.au>
2013-03-23 11:14:57 +1100

    Putting in some new documentation.

---
+++ doc_assets/js/scripts.js
@@ -0,0 +1,15 @@
+!function ($) {
+  $(function(){
+	
+	$('.dropdown-toggle').dropdown()
+	
+	$('.navbar').scrollspy()
+	
+	$(".collapse").collapse()
+	
+	 
+	
+    // make code pretty
+    window.prettyPrint && prettyPrint()
+})
+}(window.jQuery)
